b7522f83d5791cd4443c7fa92f4d3aaf5da83390
Clo91eaf <Clo91eaf@qq.com>
2024-03-16 01:33:20 +0800

    update .vscode

--- 163/C.cpp
+++ 163/C.cpp
@@ -15,18 +15,20 @@ void solve() {
   for (int i = 0; i < n; i++) {
     a[1][i] = s[i] == '>';
   }
-  
+
   dp[0][0] = 1;
   dp[1][0] = 0;
 
   for (int i = 1; i < n; i++) {
-    dp[0][i] = dp[1][i-1] && a[0][i-1];
-    if (i > 1) dp[0][i] |= dp[0][i-2] && a[0][i-1];
-    dp[1][i] = dp[0][i-1] && a[1][i-1];
-    if (i > 1) dp[1][i] |= dp[1][i-2] && a[1][i-1];
+    dp[0][i] = dp[1][i - 1] && a[0][i - 1];
+    if (i > 1)
+      dp[0][i] |= dp[0][i - 2] && a[0][i - 1];
+    dp[1][i] = dp[0][i - 1] && a[1][i - 1];
+    if (i > 1)
+      dp[1][i] |= dp[1][i - 2] && a[1][i - 1];
   }
 
-  if (dp[1][n-1]) {
+  if (dp[1][n - 1]) {
     std::cout << "YES" << std::endl;
   } else {
     std::cout << "NO" << std::endl;
