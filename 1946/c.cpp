4a17fb860d50b194c5e5cdcb93a71cbc1a4b275d
Clo91eaf <Clo91eaf@qq.com>
2024-03-22 22:30:59 +0800

    add template

---
+++ 1946/c.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
