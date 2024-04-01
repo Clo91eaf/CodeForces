910a84eaa10a30ce3c001ec188a9f8e6363d85d9
Clo91eaf <Clo91eaf@qq.com>
2024-04-01 20:22:07 +0800

    补f

--- 1950/f.cpp
+++ 1950/f.cpp
@@ -1,7 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {}
+void solve() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  if (a + 1 != c) {
+    cout << -1 << "\n";
+    return;
+  }
+
+  queue<int> q;
+  q.push(0);
+  int ans = 0;
+  while (!q.empty()) {
+    ans = q.front();
+    q.pop();
+    if (a) {
+      q.push(ans + 1);
+      q.push(ans + 1);
+      a--;
+    } else if (b) {
+      q.push(ans + 1);
+      b--;
+    }
+  }
+
+  cout << ans << "\n";
+}
 
 int main() {
   std::ios::sync_with_stdio(false);
